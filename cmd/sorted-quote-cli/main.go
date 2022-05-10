1e780f6fbbc02f2095d9c3d6d3d0dde174b0defa
Andre Jacobs <andrejacobs@users.noreply.github.com>
2022-05-10 10:17:13 +0100

    Added the latest JSON encoding support that was added by go-test1 module.

--- cmd/sorted-quote-cli/main.go
+++ cmd/sorted-quote-cli/main.go
@@ -6,28 +6,34 @@ import (
 	"io"
 	"os"
 
+	"github.com/andrejacobs/go-test1/jsonquote"
 	"github.com/andrejacobs/go-test2/sorted"
 )
 
 func main() {
-	countFlag := flag.Int("count", 1, "The number of quotes to print on STDOUT")
+	countFlag := flag.Int("count", 1, "The number of quotes to print")
+	jsonFlag := flag.Bool("json", false, "Encode the quotes into JSON")
 	flag.Parse()
 
-	if err := run(os.Stdout, *countFlag); err != nil {
+	if err := run(os.Stdout, *countFlag, *jsonFlag); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(out io.Writer, count int) error {
+func run(out io.Writer, count int, outputJson bool) error {
 	ql := &sorted.QuoteList{}
 	ql.Generate(count)
 	ql.Sort()
 
-	for _, quote := range ql.Quotes {
-		_, err := fmt.Fprintf(out, "%q - %s\n", quote.Line, quote.Who)
-		if err != nil {
-			return err
+	if outputJson {
+		jsonquote.Encode(out, &ql.QuoteList)
+	} else {
+		for _, quote := range ql.Quotes {
+			_, err := fmt.Fprintf(out, "%q - %s\n", quote.Line, quote.Who)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/andrejacobs/go-test2
 
 go 1.18
 
-require github.com/andrejacobs/go-test1 v0.0.0-20220509114742-d186c44a2d88 // indirect
+require github.com/andrejacobs/go-test1 v0.0.0-20220510085007-38df90522ed9 // indirect
